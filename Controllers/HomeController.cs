fa99d92bac20cc0d617417ecb72e494b4307be14
PatZino <eedawarez@gmail.com>
2023-04-09 17:11:56 +0100

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using jquery_ajax_aspnet_core.Models;
+
+namespace jquery_ajax_aspnet_core.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public int Add(int number1, int number2)
+        {
+            return number1 + number2;
+        }
+
+        [HttpPost]
+        public Numbers Calculate(int number1, int number2)
+        {
+            try
+            {
+                Numbers numbers = new Numbers();
+                numbers.Add = number1 + number2;
+                numbers.Substract = number1 - number2;
+                numbers.Multiply = number1 * number2;
+                numbers.Divide = (decimal)number1 / number2;
+                numbers.Divide = (decimal)number1 / 0;
+
+                return numbers;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+        }
+    }
+}
---
+++ Models/Numbers.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace jquery_ajax_aspnet_core.Models
+{
+    public class Numbers
+    {
+        public int Add { get; set; }
+        public int Substract { get; set; }
+        public int Multiply { get; set; }
+        public decimal Divide { get; set; }
+    }
+}
